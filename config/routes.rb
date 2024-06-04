8b2d62b933f0658ed0c7b4e68b1ee24f4ad397aa
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-06-04 17:53:16 +0100

    venues show and cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'venues/index'
-  get 'venues/show'
-  get 'venues/new'
-  get 'venues/create'
-  get 'venues/edit'
-  get 'venues/update'
-  get 'venues/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
