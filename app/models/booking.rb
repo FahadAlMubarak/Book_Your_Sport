6c8c16a9bb54d95fd69d69a56b368ef53d998a01
Fahad Al-mubarak <fahadalmubarak1@outlook.com>
2024-06-11 14:22:15 +0100

    refactor_review

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,15 @@ class Booking < ApplicationRecord
   has_many :slots
   has_one :review
   has_many :shared_bookings
+
+  # all the slots in the booking will belong to the same facility, therefore we can call .first
+  def venue
+    slots.map { |slot| slot.facility.venue }.first
+  end
+
+  def venue_address
+    return nil unless venue
+
+    venue.address
+  end
 end
